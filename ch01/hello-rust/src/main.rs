c4dadb5305be7b611673654f1d4bb51aad381e7d
host1812 <host1812@gmail.com>
2022-11-10 20:59:53 -0800

    ch01 start

---
+++ ch01/hello-rust/src/main.rs
@@ -0,0 +1,9 @@
+#![allow(unused_variables)]
+fn main() {
+    // let x = 0;
+    let list = [1,2];
+    println!("Hello, world!");
+    println!("first: {},\nsecond: {}", list[0], list[1]);
+    let tuple = ("bla", 10, 4.5);
+    println!("1: {}, 2: {}, 3: {}", tuple.0, tuple.1, tuple.2);
+}
